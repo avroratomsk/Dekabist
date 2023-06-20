fbc72e31c96e98fa0b50e470c7439f566ff6f7b2
OmetovVlad <ometov96@mail.ru>
2023-06-20 14:41:15 +0700

    Отзывы, карта, Контакты

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,26 @@
-import AOS from 'aos'
+import AOS from 'aos';
+import { tns } from 'tiny-slider';
 import * as functions from "./modules/functions.js";
 
 AOS.init();
+
+tns({
+    container: '#reviews_slider',
+    items: 1,
+    loop: false,
+    swipeAngle: false,
+    speed: 400,
+    nav: false,
+    gutter: 0,
+    mouseDrag: true,
+    responsive: {
+        576: {
+            items: 2
+        },
+        768: {
+            items: 3
+        }
+    }
+});
 
 functions.isWebp();
